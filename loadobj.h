51ced5dd026b94544b04a48204b27713e9f4f57c
Vincent Deng <gdsgdmh@126.com>
2016-11-19 00:36:37 +0800

    Add files via upload

--- loadobj.h
+++ loadobj.h
@@ -15,15 +15,37 @@ private:
 	// Normal vector of a point in (x, y, z)
 	glm::vec3 normalVector;
 public:
-	objPoint(glm::vec3, glm::vec2, glm::vec3);
-	objPoint(glm::vec3);
-	objPoint(glm::vec3, glm::vec2);
-	objPoint(glm::vec3, glm::vec3);
+	objPoint(const glm::vec3&, const glm::vec2&, const glm::vec3&);
+	objPoint(const glm::vec3&);
+	objPoint(const glm::vec3&, const glm::vec2&);
+	objPoint(const glm::vec3&, const glm::vec3&);
 	// Return whether the point has texture vector information or not
 	bool isTextureVector();
 	// Return whether the point has normal vector information or not
 	bool isNormalVector();
+	// Return CoordinateVector
+	glm::vec3 getCoordinateVector() const;
+	// Return TextureVector, please first use isTextureVector()
+	// to check if TextureVector exists.
+	glm::vec2 getTextureVector() const;
+	// Return TextureVector, please first use isNormalVector()
+	// to check if NormalVector exists.
+	glm::vec3 getNormalVector() const;
 };
-// Load a vector of points from obj file
-std::vector<objPoint> loadObj(std::string);
+
+class objTriangle {
+private:
+	// Points are connected to each other
+	// in order first->second->third->first to form a triangle
+	objPoint firstPoint;
+	objPoint SecondPoint;
+	objPoint ThirdPoint;
+public:
+	objTriangle(const objPoint&, const objPoint&, const objPoint&) ;
+	objPoint getFirstPoint() const;
+    objPoint getSecondPoint() const;
+    objPoint getThirdPoint() const;
+}
+// Load a vector of triangles from obj file
+std::vector<objTriangle> loadObj(std::string);
 #endif
