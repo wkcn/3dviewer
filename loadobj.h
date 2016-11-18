cf390468b1e83603019346e0f3f26b384af3357f
Vincent Deng <gdsgdmh@126.com>
2016-11-18 20:16:34 +0800

    Add files via upload

---
+++ loadobj.h
@@ -0,0 +1,24 @@
+#ifndef OBJLOAD_H
+#define OBJLOAD_H
+#include <iostream>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <vector>
+#include <string>
+
+class objPoint {
+private:
+	// Coordinate vector of a point in (x, y, z)
+	glm::vec3 coordinateVector;
+	// Texture vector of a point in (u, v)
+	glm::vec2 textureVector;
+	// Normal vector of a point in (x, y, z)
+	glm::vec3 normalVector;
+public:
+	objPoint(glm::vec3, glm::vec2, glm::vec3);
+	objPoint(glm::vec3);
+	objPoint(glm::vec3, glm::vec2);
+	objPoint(glm::vec3, glm::vec3);
+};
+std::vector<objPoint> loadObj(std::string);
+#endif
