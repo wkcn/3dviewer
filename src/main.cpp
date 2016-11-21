fc8d3ad820e1501369f6631efd64b8014c212649
Jackie Wu <wkcn@live.cn>
2016-11-21 12:41:46 +0800

    depth_test and rotate

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,22 @@
 #include "model.h"
 using namespace std;
 
-Model md = loadObj("bb.obj");
+Model md = loadObj("wolverine_obj.obj");
 
-double CAM_X = 0, CAM_Y = 0;
-double CAM_Z = 6;
-double CAM_V = 0.3;
-double CAM_ROTATE = 0;
-double CAM_ROTATE_X = 0;
-double CAM_ROTATE_Y = 0;
-double CAM_ROTATE_A = 5;
 
+GLfloat CAM_X = 0.0f;										//图像平移分量
+GLfloat cam_z = 0.0f;
+GLfloat CAM_Y = 0.0f;
+GLfloat CAM_TX = 1.0f;										//视图大小，控制缩放
+GLfloat CAM_TY = 1.0f;
+GLfloat CAM_TZ = 1.0f;
+GLfloat CAM_OLDMY, CAM_OLDMX;									//鼠标按下的位置（x,y)
+GLfloat CAM_DELTAX, CAM_DELTAY;									//释放后，x和y移动分量
 
 void SetLight(){
 	const GLfloat am = 0.0f;
-	const GLfloat l = 1.0f;
-	GLfloat light_position[] = {3,3,3,1.0f};
+	const GLfloat l = 0.5f;
+	GLfloat light_position[] = {50,50,50,1.0f};
 	GLfloat light_ambient[] = {am, am, am, 1.0f};
 	GLfloat light_diffuse[] = {l,l,l,l};
 	GLfloat light_specular[] = {l,l,l,l};
@@ -33,16 +34,53 @@ void SetLight(){
 	glEnable(lid);
 }
 
-void Display(){
-	glEnable(GL_DEPTH_TEST);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void DrawCube(){
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(CAM_X, CAM_Y, CAM_Z, CAM_X, CAM_Y, 0, 0, 1, 0);
-	glRotated(CAM_ROTATE, 0, 0, 1);
-	glRotated(CAM_ROTATE_X, 1, 0, 0);
-	glRotated(CAM_ROTATE_Y, 0, 1, 0);
+	glBegin(GL_QUADS);									// Draw A Quad
+		glColor3f(0.0f, 1.0f, 0.0f);					// Set The Color To Green
+		glVertex3f(1.0f, 1.0f, -1.0f);					// Top Right Of The Quad (Top)
+		glVertex3f(-1.0f, 1.0f, -1.0f);					// Top Left Of The Quad (Top)
+		glVertex3f(-1.0f, 1.0f, 1.0f);					// Bottom Left Of The Quad (Top)
+		glVertex3f(1.0f, 1.0f, 1.0f);					// Bottom Right Of The Quad (Top)
+		glColor3f(1.0f, 0.5f, 0.0f);						// Set The Color To Orange
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Top Right Of The Quad (Bottom)
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Top Left Of The Quad (Bottom)
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Bottom Left Of The Quad (Bottom)
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Bottom Right Of The Quad (Bottom)
+		glColor3f(1.0f, 0.0f, 0.0f);						// Set The Color To Red
+		glVertex3f(1.0f, 1.0f, 1.0f);					// Top Right Of The Quad (Front)
+		glVertex3f(-1.0f, 1.0f, 1.0f);					// Top Left Of The Quad (Front)
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Bottom Left Of The Quad (Front)
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Bottom Right Of The Quad (Front)
+		glColor3f(1.0f, 1.0f, 0.0f);						// Set The Color To Yellow
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Top Right Of The Quad (Back)
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Top Left Of The Quad (Back)
+		glVertex3f(-1.0f, 1.0f, -1.0f);					// Bottom Left Of The Quad (Back)
+		glVertex3f(1.0f, 1.0f, -1.0f);					// Bottom Right Of The Quad (Back)
+		glColor3f(0.0f, 0.0f, 1.0f);						// Set The Color To Blue
+		glVertex3f(-1.0f, 1.0f, 1.0f);					// Top Right Of The Quad (Left)
+		glVertex3f(-1.0f, 1.0f, -1.0f);					// Top Left Of The Quad (Left)
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Bottom Left Of The Quad (Left)
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Bottom Right Of The Quad (Left)
+		glColor3f(1.0f, 0.0f, 1.0f);						// Set The Color To Violet
+		glVertex3f(1.0f, 1.0f, -1.0f);					// Top Right Of The Quad (Right)
+		glVertex3f(1.0f, 1.0f, 1.0f);					// Top Left Of The Quad (Right)
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Bottom Left Of The Quad (Right)
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Bottom Right Of The Quad (Right)
+	glEnd();											// Done Drawing The Quad
+
+}
+
+void Display(){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glEnable(GL_DEPTH_TEST);
+
+	glPushMatrix();
+	glRotatef(CAM_DELTAX, 0.0, 1.0, 0.0);					//x轴旋转
+	glRotatef(CAM_DELTAY, 1.0, 0.0, 0.0);					//y轴旋转
+	glTranslatef(CAM_X, CAM_Y, cam_z);
+	glScalef(CAM_TX, CAM_TY, CAM_TZ);
+
 
 
 
@@ -52,7 +90,9 @@ void Display(){
 	// 面模型
 	SetLight();
 	md.Draw();
+	DrawCube();
 
+	glPopMatrix();
 	glutSwapBuffers();
 }
 
@@ -61,27 +101,58 @@ void Idle(){
 }
 
 void Reshape(int w, int h){
-	glViewport (0, 0, (GLsizei) w, (GLsizei) h);   
-	glMatrixMode (GL_PROJECTION);  
-	glLoadIdentity ();  
-	gluPerspective(45.0, (GLfloat) w/(GLfloat) h, 0, 10.0);  
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-2, 2, -2, 2, -10, 10);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
+void SpecialKeys(int key, int x, int y){
+	if (key == GLUT_KEY_UP)
+		CAM_Y += 0.1f;
+
+	if (key == GLUT_KEY_DOWN)
+		CAM_Y -= 0.1f;
+
+	if (key == GLUT_KEY_LEFT)
+		CAM_X -= 0.1f;
+
+	if (key == GLUT_KEY_RIGHT)
+		CAM_X += 0.1f;
+	glutPostRedisplay();
+}
+
+
 void Keyboard(unsigned char key, int x, int y){
-	switch (key){
-		case 'w':CAM_Y += CAM_V;break;
-		case 's':CAM_Y -= CAM_V;break;
-		case 'a':CAM_X -= CAM_V;break;
-		case 'd':CAM_X += CAM_V;break;
-		case 'z':CAM_Z += CAM_V;break;
-		case 'c':CAM_Z -= CAM_V;break;
-		case 'q':CAM_ROTATE -= CAM_ROTATE_A;break;
-		case 'e':CAM_ROTATE += CAM_ROTATE_A;break;
-		case 'i':CAM_ROTATE_X += CAM_ROTATE_A;break;
-		case 'o':CAM_ROTATE_X -= CAM_ROTATE_A;break;
-		case 'j':CAM_ROTATE_Y += CAM_ROTATE_A;break;
-		case 'k':CAM_ROTATE_Y -= CAM_ROTATE_A;break;
+	switch (key) {
+	case '+':												//键盘中'+'就是'='
+	case '=':
+		CAM_TX = CAM_TX * 1.1;
+		CAM_TY = CAM_TY * 1.1;
+		CAM_TZ = CAM_TZ * 1.1;
+		glutPostRedisplay();
+		break;
+	case '-':
+		CAM_TX = CAM_TX / 1.1;
+		CAM_TY = CAM_TY / 1.1;
+		CAM_TZ = CAM_TZ / 1.1;
+		glutPostRedisplay();
+		break;
 	}
+}
+
+void Mouse(int button, int state, int x, int y){ //处理鼠标点击
+	if (state == GLUT_DOWN) //第一次鼠标按下时,记录鼠标在窗口中的初始坐标  
+		CAM_OLDMX = x, CAM_OLDMY = y;
+}
+
+void OnMouseMove(int x, int y){ //处理鼠标拖动  
+	CAM_DELTAX += 360 * (x - CAM_OLDMX) / 600;
+	CAM_DELTAY += 360 * (y - CAM_OLDMY) / 600;
+	CAM_OLDMX = x;
+	CAM_OLDMY = y;
 }
 
 
@@ -118,23 +189,26 @@ void Init(){
 	//glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
 
 	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_LIGHTING);
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
 }
 
 int main(int argc, char *argv[]){
-
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowPosition(300, 100);
 	glutInitWindowSize(800, 800);
 	glutCreateWindow("3dviewer");
 
 	Init();
-	glutDisplayFunc(&Display);
-	glutIdleFunc(&Idle);
+
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
 	glutReshapeFunc(Reshape);
-	glutKeyboardFunc(&Keyboard);
+	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(SpecialKeys);
+	glutMouseFunc(Mouse);
+	glutMotionFunc(OnMouseMove);
 
 	glutMainLoop();
 	return 0;
